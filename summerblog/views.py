155cacf0d594e4cc8a5665308ea4dce9d77b749f
Trevor Scheitrum <tascheitrum@gmail.com>
2014-04-04 18:07:50 -0400

    changed view.py to reflect model

--- summerblog/views.py
+++ summerblog/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from summerblog.models import Article, Author, Photo
+from summerblog.models import Article, Author, Photo2
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, redirect
